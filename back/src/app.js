56f1d4c0a77014861a2677a0096730f85e27d821
spawn22 <todococo14@gmail.com>
2023-07-19 11:40:50 -0300

    add cors

--- back/src/app.js
+++ back/src/app.js
@@ -1,11 +1,15 @@
 import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 import authRoutes from "./routes/auth.routes.js";
 import animalRoutes from "./routes/animal.routes.js";
 import shiftRoutes from "./routes/shift.routes.js";
 
 const app = express();
+
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
