66cf7f990aed4e818b8b8945d3dc2208ece44d99
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-08-02 02:58:46 -0300

    Se cambia la etiquta main por un section por cuestion de semantica

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -4,10 +4,10 @@ import AnimalForm from "./AnimalForm";
 const Home = () => {
   return (
     // <main className="flex justify-center items-center h-screen max-h-[55rem]">
-    <main className="container m-auto md:grid md:grid-cols-2 justify-center h-screen max-h-[55rem] gap-1">
+    <section className="container m-auto md:grid md:grid-cols-2 justify-center h-100 gap-1">
       <AnimalForm />
       <AnimalCards />
-    </main>
+    </section>
   );
 };
 export default Home;
