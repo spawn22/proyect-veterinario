3d458f11cfdee3ce12850082ad382127c0ac42ba
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-08-04 00:47:31 -0300

    Se agrega el paginado para los pacientes

--- front/src/pages/Home/AnimalCards.jsx
+++ front/src/pages/Home/AnimalCards.jsx
@@ -1,12 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAnimalStore } from "../../store/animalStore";
 import AnimalCard from "./AnimalCard";
 import toast from "react-hot-toast";
+import PaginationTable from "../../components/PaginationTable"
 const AnimalCards = () => {
   const { patients } = useAnimalStore((state) => ({
     patients: state.patients,
   }));
   const { getPatients } = useAnimalStore();
+  const ITEMS_PER_PAGE = 6;
+  const [currentPage, setCurrentPage] = useState(1);
+  const shiftedPatients = patients?.slice(
+    currentPage > 1 ? (currentPage - 1) * ITEMS_PER_PAGE : 0,
+    currentPage > 1 ? currentPage * ITEMS_PER_PAGE : ITEMS_PER_PAGE
+  );
+  const totalPages = Math.ceil(
+    (patients?.length - ITEMS_PER_PAGE) / ITEMS_PER_PAGE + 1
+  );
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
 
   useEffect(() => {
     toast.promise(
@@ -37,12 +50,19 @@ const AnimalCards = () => {
       </h1>
 
       <ul className=" grid gap-4 md:grid-cols-2 mt-2">
-        {patients.map((patient) => (
+        {shiftedPatients.map((patient) => (
           <li key={patient._id}>
             <AnimalCard patient={patient} key={patient._id} />
           </li>
         ))}
       </ul>
+      {patients?.length > ITEMS_PER_PAGE && (
+              <PaginationTable
+                currentPage={currentPage}
+                totalPages={totalPages}
+                onPageChange={handlePageChange}
+              />
+            )}
     </div>
   );
 };
