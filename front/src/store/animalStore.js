fd56aef21efce4c4b5bcc2289ea39f1e95d636cc
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-08-07 12:33:18 -0300

    Se corrige la funcion, se remplaza getPatients por getAnimals

--- front/src/store/animalStore.js
+++ front/src/store/animalStore.js
@@ -64,7 +64,7 @@ export const useAnimalStore = create(
      }));
  
        if(patients.length === 0 || name === ''){
-         get().getPatients()
+         get().getAnimals()
        }
    },
   }))
