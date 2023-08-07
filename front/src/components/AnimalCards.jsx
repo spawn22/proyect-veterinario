21671bda3de29eb8df98ab5f50b942e9b7b29a61
spawn22 <todococo14@gmail.com>
2023-08-07 10:14:39 -0300

    add pagination

--- front/src/components/AnimalCards.jsx
+++ front/src/components/AnimalCards.jsx
@@ -1,14 +1,46 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAnimalStore } from "../store/animalStore";
+import PaginationTable from "../components/PaginationTable";
 import AnimalCard from "./AnimalCard";
+import toast from "react-hot-toast";
 const AnimalCards = () => {
   const { patients } = useAnimalStore((state) => ({
     patients: state.patients,
   }));
   const { getAnimals } = useAnimalStore();
+  const ITEMS_PER_PAGE = 6;
+  const [currentPage, setCurrentPage] = useState(1);
+  const shiftedPatients = patients?.slice(
+    currentPage > 1 ? (currentPage - 1) * ITEMS_PER_PAGE : 0,
+    currentPage > 1 ? currentPage * ITEMS_PER_PAGE : ITEMS_PER_PAGE
+  );
+  const totalPages = Math.ceil(
+    (patients?.length - ITEMS_PER_PAGE) / ITEMS_PER_PAGE + 1
+  );
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
 
   useEffect(() => {
-    getAnimals();
+    toast.promise(
+      getAnimals(),
+      {
+        loading: "Cargando pacientes...",
+        success: "Pacientes cargados",
+        error: "Error al cargar pacientes",
+      },
+      {
+        success: {
+          duration: 2000,
+        },
+        loading: {
+          duration: 1000,
+        },
+        error: {
+          duration: 2000,
+        },
+      }
+    );
   }, [getAnimals]);
 
   return (
@@ -18,12 +50,19 @@ const AnimalCards = () => {
       </h1>
 
       <ul className=" grid gap-6 md:grid md:grid-cols-1 lg:grid-cols-1 lg:gap-4 xl:grid xl:grid-cols-2 2xl:grid 2xl:grid-col-3 mt-2">
-        {patients.map((patient) => (
+        {shiftedPatients.map((patient) => (
           <li key={patient.id}>
             <AnimalCard patient={patient} />
           </li>
         ))}
       </ul>
+      {patients?.length > ITEMS_PER_PAGE && (
+        <PaginationTable
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+        />
+      )}
     </div>
   );
 };
