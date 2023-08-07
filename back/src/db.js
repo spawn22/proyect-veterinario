305bb2962a77a36185d05587e8ec86624fa2f487
spawn22 <todococo14@gmail.com>
2023-08-07 12:08:20 -0300

    db

--- back/src/db.js
+++ back/src/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/veterinaria-proyect");
+    await mongoose.connect(
+      "mongodb+srv://zaitsez2014:WHezH19UPOdJaxTB@cluster0.cm8iynm.mongodb.net/"
+    );
     console.log("DB is connected");
   } catch (error) {
     console.log(error);
