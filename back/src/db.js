936f1ca9f4ba39e2b06a0c85557f170623722f73
spawn22 <todococo14@gmail.com>
2023-08-07 12:18:24 -0300

    db

--- back/src/db.js
+++ back/src/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDb = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://zaitsez2014:WHezH19UPOdJaxTB@cluster0.cm8iynm.mongodb.net/"
+      "mongodb+srv://zaitsez2014:WHezH19UPOdJaxTB@cluster0.cm8iynm.mongodb.net/?retryWrites=true&w=majority"
     );
     console.log("DB is connected");
   } catch (error) {
