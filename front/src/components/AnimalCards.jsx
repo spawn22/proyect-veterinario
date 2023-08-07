aedf4f6eb68da487f253d27c44bade0668eda921
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-08-07 13:18:03 -0300

    Se corrige el use del useEffect en los buscadores

--- front/src/components/AnimalCards.jsx
+++ front/src/components/AnimalCards.jsx
@@ -12,7 +12,9 @@ const AnimalCards = () => {
   const ITEMS_PER_PAGE = 4;
   const [currentPage, setCurrentPage] = useState(1);
   const { filterAnimalsName } = useAnimalStore();
+  const { filterAnimalsType } = useAnimalStore();
   const [searchTerm, setSearchTerm] = useState('');
+  const [searchTermType, setSearchTermType] = useState('');
   const shiftedPatients = patients?.slice(
     currentPage > 1 ? (currentPage - 1) * ITEMS_PER_PAGE : 0,
     currentPage > 1 ? currentPage * ITEMS_PER_PAGE : ITEMS_PER_PAGE
@@ -26,10 +28,17 @@ const AnimalCards = () => {
   const handleSearch = (e) =>{
     setSearchTerm(e.target.value)
   }
+  const handleSearchType = (e) =>{
+    setSearchTermType(e.target.value)
+  }
 
   useEffect(()=>{
     filterAnimalsName(searchTerm)
-  },[searchTerm, filterAnimalsName])
+  },[searchTerm, filterAnimalsName, searchTermType, filterAnimalsType])
+  
+  useEffect(()=>{
+    filterAnimalsType(searchTermType)
+  },[searchTermType, filterAnimalsType])
 
   useEffect(() => {
     toast.promise(
@@ -61,12 +70,21 @@ const AnimalCards = () => {
       <div className=" mr-4">
           <Input
             type="text"
-            placeholder="Buscar nombre Paciente"
+            placeholder="Nombre de mascota"
             className="px-2 py-1 border border-gray-300 rounded-md w-[100%]"
             value={searchTerm}
             onChange={handleSearch}
           />
-        </div>
+      </div>
+      <div className=" mr-4">
+          <Input
+            type="text"
+            placeholder="Tipo de animal, Ej: Gato, Perro, etc."
+            className="px-2 py-1 border border-gray-300 rounded-md w-[100%]"
+            value={searchTermType}
+            onChange={handleSearchType}
+          />
+      </div>
       <ul className=" grid gap-6 md:grid md:grid-cols-1 lg:grid-cols-1 lg:gap-4 xl:grid xl:grid-cols-2 2xl:grid 2xl:grid-col-3 mt-2">
         {shiftedPatients.map((patient) => (
           <li key={patient.id}>
