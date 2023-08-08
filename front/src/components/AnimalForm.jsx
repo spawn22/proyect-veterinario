966f725ce948ac5f34eff0e787a439fe08192199
Alejandro Anibal Isa <alejandroisa71@gmail.com>
2023-08-08 19:36:34 -0300

    Add funcionality editing Animal

--- front/src/components/AnimalForm.jsx
+++ front/src/components/AnimalForm.jsx
@@ -1,11 +1,13 @@
 import useErrors from "../hooks/useErrors";
-import { useAnimalStore } from "../store/animalStore";
+import { useAnimalStore, getAnimalById } from "../store/animalStore";
 import toast from "react-hot-toast";
 import useForm from "../hooks/useForm";
 import { Input } from "./Input";
 import Label from "./Label";
 import Button from "./Button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+
 const initialValues = {
   name: "",
   owner: "",
@@ -19,27 +21,46 @@ const initialValues = {
 
 const AnimalForm = () => {
   const form = useForm({ initialValues });
-
+  const params = useParams();
+  const navigate = useNavigate();
+  const [edit, setEdit] = useState(false);
+  const { id } = params;
   const { error, setError } = useErrors();
 
   const registerAnimal = useAnimalStore((state) => state.registerAnimal);
+  const animalToEdit = useAnimalStore(getAnimalById(id));
+  const putAnimal = useAnimalStore((state) => state.putAnimal);
 
   // Se define un estado para el texto del botón que indica cuando se está cargando
   const [isButtonLoading, setIsButtonLoading] = useState(false);
 
+  useEffect(() => {
+  //Compara si es edición o registro
+    if (params.id) {
+      setEdit(true);
+      const { setFields } = form;
+      setFields(animalToEdit);
+    } else {
+      setEdit(false);
+    }
+  }, [params]);
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     const { fields, setFields } = form;
+    console.log(fields);
     //Valida que estén llenos todos los inputs
     if (
       !fields.name.trim() ||
       !fields.owner.trim() ||
       !fields.type.trim() ||
-      !fields.age.trim() ||
       !fields.gender.trim() ||
       !fields.breed.trim() ||
-      !fields.weight.trim() ||
-      !fields.description.trim()
+      !fields.description.trim() ||
+      (!edit && (!fields.age.trim() ||
+      !fields.weight.trim())) ||
+      (edit && !String(fields.age).trim() ||
+      !String(fields.weight).trim())
     ) {
       setError(true);
       toast.error("Todos los campos son obligatorios", error);
@@ -56,18 +77,26 @@ const AnimalForm = () => {
       description: fields.description,
     };
     setIsButtonLoading(true);
-    await registerAnimal(data);
-    toast.success("Animal registrado con éxito");
+    if (edit) {
+      const dataEdit = { ...data, id };
+      console.log(dataEdit);
+      await putAnimal(dataEdit);
+      toast.success("Animal modificado con éxito");
+    } else {
+      await registerAnimal(data);
+      toast.success("Animal registrado con éxito");
+    }
     setError(false);
 
     // Reinciar el form
     setIsButtonLoading(false);
     setFields(initialValues);
+    navigate("/home");
   };
   return (
     <div>
       <h1 className="text-zinc-50 mb-4 font-bold text-2xl flex gap-2 mt-5 ">
-        Crea tus Pacientes{" "}
+        {!edit ? "Registra tus Pacientes" : "Edita tus Pacientes"}
         <span className="text-sky-300 ">Desde este Formulario</span>
       </h1>
       <form
@@ -75,7 +104,7 @@ const AnimalForm = () => {
         className="w-full mt-6 w-md md:w-full xl:max-w-md px-8 py-6 bg-zinc-50 rounded-lg shadow-md"
       >
         <h2 className="mb-6 text-3xl font-bold text-center text-gray-800">
-          Registrar Paciente
+          {!edit ? "Registrar Paciente" : "Editando Paciente"}
         </h2>
         <Label className="text-black block w-full text-left mb-2">
           Nombre
@@ -158,11 +187,20 @@ const AnimalForm = () => {
 
         <Button
           type="submit"
-          className="w-full px-4 py-2 mt-4 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full px-4 py-2 mt-4 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 mb-5"
           disabled={isButtonLoading}
         >
-          {isButtonLoading ? "Cargando..." : "Agregar Paciente"}
+          {isButtonLoading ? "Cargando..." : null}
+          {!edit ? "Registrar" : "Guardar"}
         </Button>
+        {edit ? (
+          <Link
+            to="/home"
+            className="px-4 py-3 bg-[#4a3dfc] w-full text-[#fffffe] rounded-md hover:bg-gray-400"
+          >
+            Cancelar
+          </Link>
+        ) : null}
       </form>
     </div>
   );
